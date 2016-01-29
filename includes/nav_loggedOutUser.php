ad47989547e218a3c9f2a2e1e3eb3ee1f1904566
Dean Connolly <dtmalfalfa@gmail.com>
2016-01-29 20:35:59 +1100

    Initial Code Sync
    Here's the initial AND OFFICIAL codebase of Webapps.io - enjoy :)

---
+++ includes/nav_loggedOutUser.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="https://bootswatch.com/cyborg/#">Brand</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="https://bootswatch.com/cyborg/#">Link <span class="sr-only">(current)</span></a></li>
+                <li><a href="https://bootswatch.com/cyborg/#">Link</a></li>
+                <li class="dropdown">
+                    <a href="https://bootswatch.com/cyborg/#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="https://bootswatch.com/cyborg/#">Action</a></li>
+                        <li><a href="https://bootswatch.com/cyborg/#">Another action</a></li>
+                        <li><a href="https://bootswatch.com/cyborg/#">Something else here</a></li>
+                        <li class="divider"></li>
+                        <li><a href="https://bootswatch.com/cyborg/#">Separated link</a></li>
+                        <li class="divider"></li>
+                        <li><a href="https://bootswatch.com/cyborg/#">One more separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="https://bootswatch.com/cyborg/#">Link</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
